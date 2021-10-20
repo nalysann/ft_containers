0231ca63d75f4dac9e7f4bded7f61a126af92198
x33nu <urbilya@gmail.com>
2021-10-20 16:40:26 +0300

    Move implementation details to a separate namespace

--- algorithm.hpp
+++ algorithm.hpp
@@ -5,6 +5,33 @@
 
 namespace ft {
 
+    namespace impl {
+
+        template <class T1, class T2 = T1>
+        struct less {
+            bool operator()(const T1& x, const T1& y) const { return x < y; }
+            bool operator()(const T1& x, const T2& y) const { return x < y; }
+            bool operator()(const T2& x, const T1& y) const { return x < y; }
+            bool operator()(const T2& x, const T2& y) const { return x < y; }
+        };
+
+        template <class T1>
+        struct less<T1, T1> {
+            bool operator()(const T1& x, const T1& y) const { return x < y; }
+        };
+
+        template <class T1>
+        struct less<const T1, T1> {
+            bool operator()(const T1& x, const T1& y) const { return x < y; }
+        };
+
+        template <class T1>
+        struct less<T1, const T1> {
+            bool operator()(const T1& x, const T1& y) const { return x < y; }
+        };
+
+    }
+
     template <class InputIterator1, class InputIterator2, class Compare>
     bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                                  InputIterator2 first2, InputIterator2 last2,
@@ -26,8 +53,8 @@ namespace ft {
                                         InputIterator2 first2, InputIterator2 last2)
     {
         return lexicographical_compare(first1, last1, first2, last2,
-                                       less<typename iterator_traits<InputIterator1>::value_type,
-                                            typename iterator_traits<InputIterator2>::value_type>());
+                                       impl::less<typename iterator_traits<InputIterator1>::value_type,
+                                                  typename iterator_traits<InputIterator2>::value_type>());
     }
 
 }
--- type_traits.hpp
+++ type_traits.hpp
@@ -33,56 +33,60 @@ namespace ft {
         typedef T type;
     };
 
-    template<class>
-    struct is_integral_helper : public false_type {};
+    namespace impl {
 
-    template<>
-    struct is_integral_helper<bool> : public true_type {};
+        template<class>
+        struct is_integral_helper : public false_type {};
 
-    template<>
-    struct is_integral_helper<char> : public true_type {};
+        template<>
+        struct is_integral_helper<bool> : public true_type {};
 
-    template<>
-    struct is_integral_helper<signed char> : public true_type {};
+        template<>
+        struct is_integral_helper<char> : public true_type {};
 
-    template<>
-    struct is_integral_helper<unsigned char> : public true_type {};
+        template<>
+        struct is_integral_helper<signed char> : public true_type {};
 
-    template<>
-    struct is_integral_helper<wchar_t> : public true_type {};
+        template<>
+        struct is_integral_helper<unsigned char> : public true_type {};
 
-    template<>
-    struct is_integral_helper<short> : public true_type {};
+        template<>
+        struct is_integral_helper<wchar_t> : public true_type {};
 
-    template<>
-    struct is_integral_helper<unsigned short> : public true_type {};
+        template<>
+        struct is_integral_helper<short> : public true_type {};
 
-    template<>
-    struct is_integral_helper<int> : public true_type {};
+        template<>
+        struct is_integral_helper<unsigned short> : public true_type {};
 
-    template<>
-    struct is_integral_helper<unsigned int> : public true_type {};
+        template<>
+        struct is_integral_helper<int> : public true_type {};
 
-    template<>
-    struct is_integral_helper<long> : public true_type {};
+        template<>
+        struct is_integral_helper<unsigned int> : public true_type {};
 
-    template<>
-    struct is_integral_helper<unsigned long> : public true_type {};
+        template<>
+        struct is_integral_helper<long> : public true_type {};
 
-    template<>
-    struct is_integral_helper<long long> : public true_type {};
+        template<>
+        struct is_integral_helper<unsigned long> : public true_type {};
 
-    template<>
-    struct is_integral_helper<unsigned long long> : public true_type {};
+        template<>
+        struct is_integral_helper<long long> : public true_type {};
 
-    template<>
-    struct is_integral_helper<__int128_t> : public true_type {};
+        template<>
+        struct is_integral_helper<unsigned long long> : public true_type {};
 
-    template<>
-    struct is_integral_helper<__uint128_t> : public true_type {};
+        template<>
+        struct is_integral_helper<__int128_t> : public true_type {};
+
+        template<>
+        struct is_integral_helper<__uint128_t> : public true_type {};
+
+    }
 
     template<class T>
-    struct is_integral : public is_integral_helper<typename remove_cv<T>::type>::type {};
+    struct is_integral : public impl::is_integral_helper<typename remove_cv<T>::type>::type {};
 
     template<bool, class T = void>
     struct enable_if {};
