4ad09b2f8c273d34a10e8fe754bfbeb596f6fa60
x33nu <urbilya@gmail.com>
2021-10-22 15:28:04 +0300

    Add equal to <algorithm>

--- algorithm.hpp
+++ algorithm.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
 #include "iterator.hpp"
-#include "functional.hpp"
 
 namespace ft {
 
     namespace impl {
+
+        template <class T1, class T2 = T1>
+        struct equal_to {
+            bool operator()(const T1& x, const T1& y) const { return x == y; }
+            bool operator()(const T1& x, const T2& y) const { return x == y; }
+            bool operator()(const T2& x, const T1& y) const { return x == y; }
+            bool operator()(const T2& x, const T2& y) const { return x == y; }
+        };
+
+        template <class T1>
+        struct equal_to<T1, T1> {
+            bool operator()(const T1& x, const T1& y) const { return x == y; }
+        };
+
+        template <class T1>
+        struct equal_to<const T1, T1> {
+            bool operator()(const T1& x, const T1& y) const { return x == y; }
+        };
+
+        template <class T1>
+        struct equal_to<T1, const T1> {
+            bool operator()(const T1& x, const T1& y) const { return x == y; }
+        };
 
         template <class T1, class T2 = T1>
         struct less {
@@ -52,9 +74,26 @@ namespace ft {
     inline bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                                         InputIterator2 first2, InputIterator2 last2)
     {
-        return lexicographical_compare(first1, last1, first2, last2,
-                                       impl::less<typename iterator_traits<InputIterator1>::value_type,
-                                                  typename iterator_traits<InputIterator2>::value_type>());
+        typedef typename iterator_traits<InputIterator1>::value_type v1;
+        typedef typename iterator_traits<InputIterator2>::value_type v2;
+        return lexicographical_compare(first1, last1, first2, last2, impl::less<v1, v2>());
+    }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+    inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred) {
+        for (; first1 != last1; ++first1, ++first2) {
+            if (!pred(*first1, *first2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <class InputIterator1, class InputIterator2>
+    inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+        typedef typename iterator_traits<InputIterator1>::value_type v1;
+        typedef typename iterator_traits<InputIterator2>::value_type v2;
+        return equal(first1, last1, first2, impl::equal_to<v1, v2>());
     }
 
 }
