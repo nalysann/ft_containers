4edb9ab620bba3627a98424b73ad38fa589b3704
x33nu <urbilya@gmail.com>
2021-10-22 17:34:07 +0300

    Qualify ptrdiff_t with std::

--- iterator.hpp
+++ iterator.hpp
@@ -23,7 +23,7 @@ namespace ft {
 
     template <class T>
     struct iterator_traits<T*> {
-        typedef ptrdiff_t difference_type;
+        typedef std::ptrdiff_t difference_type;
         typedef typename remove_cv<T>::type value_type;
         typedef T* pointer;
         typedef T& reference;
