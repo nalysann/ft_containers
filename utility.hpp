640fded756674ef57257934b9655257c8ebfb978
x33nu <urbilya@gmail.com>
2022-01-24 17:15:20 +0300

    Update pair

--- utility.hpp
+++ utility.hpp
@@ -17,44 +17,40 @@ namespace ft {
         template<class U1, class U2>
         pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
 
-        pair& operator=(const pair& p) {
-            first = p.first;
-            second = p.second;
-            return *this;
-        }
+        pair& operator=(const pair& p) { first = p.first; second = p.second; return *this; }
     };
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) {
         return x.first == y.first && x.second == y.second;
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator!=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
         return !(x == y);
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) {
-        return x.first < y.first || (!(x.first < y.first) && x.second < y.second);
+        return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator>(const pair<T1, T2>& x, const pair<T1, T2>& y) {
         return y < x;
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator<=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
         return !(y < x);
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline bool operator>=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
         return !(x < y);
     }
 
-    template<class T1, class T2>
+    template <class T1, class T2>
     inline pair<T1, T2> make_pair(T1 x, T2 y) {
         return pair<T1, T2>(x, y);
     }
