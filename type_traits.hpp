81d7a0bda0b1ae6fbdddc25024a71c639f5178fc
x33nu <urbilya@gmail.com>
2022-01-24 17:37:40 +0300

    Update type_traits

--- type_traits.hpp
+++ type_traits.hpp
@@ -2,7 +2,7 @@
 
 namespace ft {
 
-    template<class T, T v>
+    template <class T, T v>
     struct integral_constant {
         static const T value = v;
         typedef T value_type;
@@ -13,85 +13,85 @@ namespace ft {
     typedef integral_constant<bool, true> true_type;
     typedef integral_constant<bool, false> false_type;
 
-    template<class T>
+    template <class T>
     struct remove_cv {
         typedef T type;
     };
 
-    template<class T>
+    template <class T>
     struct remove_cv<const T> {
         typedef T type;
     };
 
-    template<class T>
+    template <class T>
     struct remove_cv<volatile T> {
         typedef T type;
     };
 
-    template<class T>
+    template <class T>
     struct remove_cv<const volatile T> {
         typedef T type;
     };
 
     namespace impl {
 
-        template<class>
+        template <class>
         struct is_integral_helper : public false_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<bool> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<char> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<signed char> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<unsigned char> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<wchar_t> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<short> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<unsigned short> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<int> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<unsigned int> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<long> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<unsigned long> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<long long> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<unsigned long long> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<__int128_t> : public true_type {};
 
-        template<>
+        template <>
         struct is_integral_helper<__uint128_t> : public true_type {};
 
     }
 
-    template<class T>
+    template <class T>
     struct is_integral : public impl::is_integral_helper<typename remove_cv<T>::type>::type {};
 
-    template<bool, class T = void>
+    template <bool, class T = void>
     struct enable_if {};
 
-    template<class T>
+    template <class T>
     struct enable_if<true, T> {
         typedef T type;
     };
