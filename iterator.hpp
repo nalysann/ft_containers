19f7cd60f340ad1ebfee5707514755b3c9ffc228
Ilia Urbanovich <urbilya@gmail.com>
2022-07-10 21:58:21 +0300

    Make iterator_traits more c++98 compliant

--- iterator.hpp
+++ iterator.hpp
@@ -3,8 +3,6 @@
 #include <cstddef>
 #include <iterator>
 
-#include "type_traits.hpp"
-
 namespace ft {
 
     template <class Iter>
@@ -19,9 +17,18 @@ namespace ft {
     template <class T>
     struct iterator_traits<T*> {
         typedef std::ptrdiff_t difference_type;
-        typedef typename remove_cv<T>::type value_type;
+        typedef T value_type;
         typedef T* pointer;
         typedef T& reference;
+        typedef std::random_access_iterator_tag iterator_category;
+    };
+
+    template <class T>
+    struct iterator_traits<const T*> {
+        typedef std::ptrdiff_t difference_type;
+        typedef T value_type;
+        typedef const T* pointer;
+        typedef const T& reference;
         typedef std::random_access_iterator_tag iterator_category;
     };
 
