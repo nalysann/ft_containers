5508bab6c3c3c6d2d0b1eb57e53e58f32ac97e5d
Ilia Urbanovich <urbilya@gmail.com>
2022-07-18 15:13:01 +0300

    Update stack.hpp to follow new repo structure

--- stack.hpp
+++ stack.hpp
@@ -4,66 +4,66 @@
 
 namespace ft {
 
-    template <class T, class Container = vector<T> >
-    class stack {
-    public:
-        typedef Container container_type;
-        typedef typename container_type::value_type value_type;
-        typedef typename container_type::size_type size_type;
+template <class T, class Container = vector<T> >
+class stack {
+public:
+    typedef Container container_type;
+    typedef typename container_type::value_type value_type;
+    typedef typename container_type::size_type size_type;
 
-    protected:
-        container_type c;
+protected:
+    container_type c;
 
-    public:
-        explicit stack(const Container& c = Container()) : c(c) {}
+public:
+    explicit stack(const Container& c = Container()) : c(c) {}
 
-        bool empty() const { return c.empty(); }
+    bool empty() const { return c.empty(); }
 
-        size_type size() const { return c.size(); }
+    size_type size() const { return c.size(); }
 
-        value_type& top() { return c.back(); }
+    value_type& top() { return c.back(); }
 
-        const value_type& top() const { return c.back(); }
+    const value_type& top() const { return c.back(); }
 
-        void push(const value_type& v) { c.push_back(v); }
+    void push(const value_type& v) { c.push_back(v); }
 
-        void pop() { c.pop_back(); }
+    void pop() { c.pop_back(); }
 
-        template <class T1, class C1>
-        friend bool operator==(const stack<T1, C1>& x, const stack<T1, C1>& y);
+    template <class T1, class C1>
+    friend bool operator==(const stack<T1, C1>& x, const stack<T1, C1>& y);
 
-        template <class T1, class C1>
-        friend bool operator<(const stack<T1, C1>& x, const stack<T1, C1>& y);
-    };
+    template <class T1, class C1>
+    friend bool operator<(const stack<T1, C1>& x, const stack<T1, C1>& y);
+};
 
-    template <class T, class Container>
-    inline bool operator==(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return x.c == y.c;
-    }
-
-    template <class T, class Container>
-    inline bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return !(x == y);
-    }
-
-    template <class T, class Container>
-    inline bool operator<(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return x.c < y.c;
-    }
-
-    template <class T, class Container>
-    inline bool operator>(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return y < x;
-    }
-
-    template <class T, class Container>
-    inline bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return !(y < x);
-    }
-
-    template <class T, class Container>
-    inline bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y) {
-        return !(x < y);
-    }
-
+template <class T, class Container>
+inline bool operator==(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return x.c == y.c;
 }
+
+template <class T, class Container>
+inline bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return !(x == y);
+}
+
+template <class T, class Container>
+inline bool operator<(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return x.c < y.c;
+}
+
+template <class T, class Container>
+inline bool operator>(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return y < x;
+}
+
+template <class T, class Container>
+inline bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return !(y < x);
+}
+
+template <class T, class Container>
+inline bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y) {
+    return !(x < y);
+}
+
+} // namespace ft
