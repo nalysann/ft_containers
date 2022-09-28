75f86e8b8360b0139e7ea50825c76d9d02f87cf3
Ilia Urbanovich <urbilya@gmail.com>
2022-09-28 16:13:04 +0300

    Move comp helpers to anon namespaces

--- util/equal.hpp
+++ util/equal.hpp
@@ -1,13 +1,38 @@
 #pragma once
 
-#include "util/comp.hpp"
-#include "util/iterator_traits.hpp"
+#include "iterator_traits.hpp"
+
+namespace {
+
+template <class T1, class T2 = T1>
+struct equal_to {
+    bool operator()(const T1& x, const T1& y) const { return x == y; }
+    bool operator()(const T1& x, const T2& y) const { return x == y; }
+    bool operator()(const T2& x, const T1& y) const { return x == y; }
+    bool operator()(const T2& x, const T2& y) const { return x == y; }
+};
+
+template <class T1>
+struct equal_to<T1, T1> {
+    bool operator()(const T1& x, const T1& y) const { return x == y; }
+};
+
+template <class T1>
+struct equal_to<const T1, T1> {
+    bool operator()(const T1& x, const T1& y) const { return x == y; }
+};
+
+template <class T1>
+struct equal_to<T1, const T1> {
+    bool operator()(const T1& x, const T1& y) const { return x == y; }
+};
+
+} // anonymous namespace
 
 namespace ft {
 
 template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
-{
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred) {
     for (; first1 != last1; ++first1, ++first2) {
         if (!pred(*first1, *first2)) {
             return false;
@@ -17,11 +42,10 @@ bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, B
 }
 
 template <class InputIterator1, class InputIterator2>
-bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-{
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
     typedef typename iterator_traits<InputIterator1>::value_type v1;
     typedef typename iterator_traits<InputIterator2>::value_type v2;
-    return equal(first1, last1, first2, impl::equal_to<v1, v2>());
+    return equal(first1, last1, first2, equal_to<v1, v2>());
 }
 
 } // namespace ft
--- util/lexicographical_compare.hpp
+++ util/lexicographical_compare.hpp
@@ -1,15 +1,40 @@
 #pragma once
 
-#include "util/comp.hpp"
-#include "util/iterator_traits.hpp"
+#include "iterator_traits.hpp"
+
+namespace {
+
+template <class T1, class T2 = T1>
+struct less {
+    bool operator()(const T1& x, const T1& y) const { return x < y; }
+    bool operator()(const T1& x, const T2& y) const { return x < y; }
+    bool operator()(const T2& x, const T1& y) const { return x < y; }
+    bool operator()(const T2& x, const T2& y) const { return x < y; }
+};
+
+template <class T1>
+struct less<T1, T1> {
+    bool operator()(const T1& x, const T1& y) const { return x < y; }
+};
+
+template <class T1>
+struct less<const T1, T1> {
+    bool operator()(const T1& x, const T1& y) const { return x < y; }
+};
+
+template <class T1>
+struct less<T1, const T1> {
+    bool operator()(const T1& x, const T1& y) const { return x < y; }
+};
+
+} // anonymous namespace
 
 namespace ft {
 
 template <class InputIterator1, class InputIterator2, class Compare>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                              InputIterator2 first2, InputIterator2 last2,
-                             Compare comp)
-{
+                             Compare comp) {
     for (; first2 != last2; ++first1, ++first2) {
         if (first1 == last1 || comp(*first1, *first2)) {
             return true;
@@ -23,11 +48,10 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 
 template <class InputIterator1, class InputIterator2>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
-                                    InputIterator2 first2, InputIterator2 last2)
-{
+                             InputIterator2 first2, InputIterator2 last2) {
     typedef typename iterator_traits<InputIterator1>::value_type v1;
     typedef typename iterator_traits<InputIterator2>::value_type v2;
-    return lexicographical_compare(first1, last1, first2, last2, impl::less<v1, v2>());
+    return lexicographical_compare(first1, last1, first2, last2, less<v1, v2>());
 }
 
 } // namespace ft
